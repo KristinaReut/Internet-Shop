05f12b5b1843015b2b92a6c192fae9cac4640fc3
Kristina <kristina241996@mail.ru>
2018-07-16 13:52:07 +0300

    change function

--- Admin/categories.js
+++ Admin/categories.js
@@ -1,64 +1,59 @@
 
-$(function () {
-
-//adding category
-$(".btn-add-category").click(function (e) {
-    //prevent Default functionality
-    e.preventDefault();
-    var addCategory = $("#add-categoty").val();
-    var tableLength = $("#table-category tr").length;
-    if (addCategory.length > 0) {
-        $.ajax({
-            url: 'http://localhost:2403/category/',
-            type: 'POST',
-            data: { name: addCategory },
+$(document).ready(function () {
+    $.get('http://localhost:2403/category/', function (data) {
+        console.log('ready', data);
+        $(".btn-add-category").click(function (e) {
+            e.preventDefault();
+            var addCategory = $("#add-categoty").val();
+            var tableLength = $("#table-category tr").length;
+            if (addCategory.length > 0) {
+                $.ajax({
+                    url: 'http://localhost:2403/category/',
+                    type: 'POST',
+                    data: { name: addCategory },
+                });
+                $("#table-category").append('<tr class="categ"> <th>' + tableLength + '</th><th class="categor">' + addCategory + '</th><th><button type="button" class="btn btn-delete btn-delete-category">Delete</button><button type="button" class="btn btn-edit btn-edit-category">Edit</button></th></tr>');
+                $("form").get(0).reset();
+                $("#category").append('<option>' + addCategory + '</option');
+                $(".category-filter").append('<option>' + addCategory + '</option');
+            }
         });
-        $("#table-category").append('<tr class="categ"> <th>' + tableLength + '</th><th class="categor">' + addCategory + '</th><th><button type="button" class="btn btn-delete btn-delete-category">Delete</button><button type="button" class="btn btn-edit btn-edit-category">Edit</button></th></tr>');
-        $("form").get(0).reset(); 
-    }
-}); 	
-//btn-delete
-	$('body').on('click','.btn-delete',(function(e){
-		let tableDelete = $(e.target).parents('.categ').remove();
-		console.log(tableDelete)
-		$.ajax({
-			url: 'http://localhost:2403/category/'+ tableDelete,
-			type: 'DELETE',
-			success: function(result){
-				$(e.target).parents('.categ').remove();
-			}
-		})
-		$.ajax({
-			url: 'http://localhost:2403/products/'+ tableDelete,
-			type: 'DELETE',
-			success: function(result){
-				$(e.target).parents('.prod').remove();
-			}
-		})
-	}));
-//btn-edit
-	$('body').on('click', '.btn-edit', (function(eb){
 
-		$.ajax({
-			url: 'http://localhost:2403/category/'+ trId,
-			type: 'PUT',
-			success: function(result){
-			
-			}
-		})
-		$.ajax({
-			url: 'http://localhost:2403/products/'+ trId,
-			type: 'PUT',
-			success: function(result){
-				
-			}
-		})
-	}));
+        //btn-delete
+        $('body').on('click', '.btn-delete', (function (e) {
+            let tableDelete = $(".categ").attr("id");
+            console.log(tableDelete)
+            $.ajax({
+                url: 'http://localhost:2403/category/' + tableDelete,
+                type: 'DELETE',
+            });
+        }));
 
-// $.ajax({
-//     url: 'http://localhost:2403/dashboard/category/',
-//     data: name,
-//     success: success,
-//     dataType: dataType
-// });
+        //btn-edit
+        $('body').on('click', '.btn-edit', (function (eb) {
+
+            $.ajax({
+                url: 'http://localhost:2403/category/' + trId,
+                type: 'PUT',
+                success: function (result) {
+
+                }
+            })
+            $.ajax({
+                url: 'http://localhost:2403/products/' + trId,
+                type: 'PUT',
+                success: function (result) {
+
+                }
+            })
+        }));
+
+        // $.ajax({
+        //     url: 'http://localhost:2403/dashboard/category/',
+        //     type: GET,
+        //     data: name,
+        //     success: success,
+        //     dataType: dataType
+        // });
+    });
 });
