d9ac8fec9ec61b772694b8f8f082b7ad3744e58a
Kristina <kristina241996@mail.ru>
2018-07-16 16:01:36 +0300

    change functions

--- Admin/categories.js
+++ Admin/categories.js
@@ -2,9 +2,18 @@
 $(document).ready(function () {
     $.get('http://localhost:2403/category/', function (data) {
         console.log('ready', data);
+        
         $(".btn-add-category").click(function (e) {
             e.preventDefault();
             var addCategory = $("#add-categoty").val();
+            var row = data.map(function(elem, index){
+                //console.log('<tr><td>' + index + '</td><td>' + elem.name + '</td></tr>');
+                //console.log(elem)
+                var indexNew = index + 1;
+                $("#table-category").html("");
+                $("#table-category").append('<tr class="categ"> <td>' + indexNew + '</td><td class="categor">' + elem.name + '</td><td><button type="button" class="btn btn-delete btn-delete-category">Delete</button><button type="button" class="btn btn-edit btn-edit-category">Edit</button></td></tr>');
+            })
+
             var tableLength = $("#table-category tr").length;
             if (addCategory.length > 0) {
                 $.ajax({
@@ -12,7 +21,7 @@ $(document).ready(function () {
                     type: 'POST',
                     data: { name: addCategory },
                 });
-                $("#table-category").append('<tr class="categ"> <th>' + tableLength + '</th><th class="categor">' + addCategory + '</th><th><button type="button" class="btn btn-delete btn-delete-category">Delete</button><button type="button" class="btn btn-edit btn-edit-category">Edit</button></th></tr>');
+                
                 $("form").get(0).reset();
                 $("#category").append('<option>' + addCategory + '</option');
                 $(".category-filter").append('<option>' + addCategory + '</option');
@@ -30,7 +39,7 @@ $(document).ready(function () {
         }));
 
         //btn-edit
-        $('body').on('click', '.btn-edit', (function (eb) {
+        $('body').on('click', '.btn-edit', (function (e) {
 
             $.ajax({
                 url: 'http://localhost:2403/category/' + trId,
